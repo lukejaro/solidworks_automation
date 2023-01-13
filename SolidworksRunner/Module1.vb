45c395b2fc30e11aa0a8d12a25640f96a2cdb6cd
Luke Jaroszewski <Luke.Jaroszewski@epicor.com>
2023-01-12 19:58:50 -0600

    initial deployment

---
+++ SolidworksRunner/Module1.vb
@@ -0,0 +1,47 @@
+﻿
+Imports System.CodeDom
+Imports System.ComponentModel.Design
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports SolidWorks.Interop.sldworks
+Imports SolidWorks.Interop.swconst
+
+
+Module Module1
+
+    Sub Main()
+        Dim app As SldWorks = New SldWorks()
+        Dim doc As ModelDoc2 = app.ActiveDoc
+        Debug.WriteLine(doc.GetTitle())
+
+        Dim materialMap As Dictionary(Of String, Component2) = New Dictionary(Of String, Component2)
+        Dim rootComponent As Component2 = doc.ConfigurationManager.ActiveConfiguration().GetRootComponent()
+        For Each component As Component2 In getAllSubChildren(rootComponent)
+            Debug.WriteLine(component)
+        Next
+
+    End Sub
+
+
+
+
+
+    ' Helper Functions
+    Function getAllSubChildren(component As Component2)
+        Dim componentsToReturn As List(Of Component2) = New List(Of Component2)
+        recurseChildren(component, componentsToReturn)
+        Return componentsToReturn
+    End Function
+    Function recurseChildren(component As Component2, components As List(Of Component2))
+        If Not component.GetChildren() Is Nothing Then
+            For Each child As Component In component.GetChildren()
+                components.Add(child)
+                If Not child.GetChildren() Is Nothing Then
+                    recurseChildren(child, components)
+                End If
+            Next
+        End If
+        Return components
+    End Function
+
+End Module
---
+++ SolidworksRunner/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SolidworksRunner")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Epicor Software (USA)")>
+<Assembly: AssemblyProduct("SolidworksRunner")>
+<Assembly: AssemblyCopyright("Copyright © Epicor Software (USA) 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c8887079-641d-42da-9f85-4f53cde85310")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
